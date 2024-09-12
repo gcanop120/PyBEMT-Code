ee6116c07fcc2b43a67dc3902dcf468115f4c0e4
Gerardo Cano Perea <144932076+gcanop120@users.noreply.github.com>
2024-09-12 00:24:45 -0600

    Resources folder added: YAML hydrofoils data added

---
+++ main.py
@@ -0,0 +1,6 @@
+# ==================================================================================================================================
+# PYTHON IMPLEMENTATION OF THE BEMT METHOD FOR THE DESIGN AND EVALUATION OF AN OCEAN CURRENT TURBINE // IITCA-UAEMex // 2024B // GCP
+# ==================================================================================================================================
+
+# Part 1. Airfoil Data Extrapolation by using the AirfoilPrep library to extrapolate the airfoil data.
+# The library is developed by S.A Ning NREL and is available at: https://github.com/WISDEM/AirfoilPreppy.git
