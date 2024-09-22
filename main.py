6297f4a9d607a7c514d6480ffff824bca4f0b360
Gerardo Cano Perea <144932076+gcanop120@users.noreply.github.com>
2024-09-21 19:52:15 -0600

    Preprocessing functions files completed and verifieD

--- main.py
+++ main.py
@@ -4,7 +4,16 @@
 
 # Required Libraries
 from utils.preprocessing import hydrofoils_data_check
+from utils.preprocessing import fluid_properties_data_check
+from utils.preprocessing import operative_state_data_check
 
 # Part 1. Import the required hydrofoil and turbine setup data.
 HYDROFOIL_FOLDER_PATH = "hydrofoils"
-files = hydrofoils_data_check(path=HYDROFOIL_FOLDER_PATH)
+FLUID_PROPERTIES_FILE_PATH = "turbine/fluid_properties.yml"
+OPERATIVE_STATE_FILE_PATH = "turbine/operative_state.yml"
+
+# Part 1.A Check if the hydrofoil data, fluid properties data, and operative state data
+# are available and have the correct data structure required for the analysis.
+files_hydrofoils = hydrofoils_data_check(path=HYDROFOIL_FOLDER_PATH)
+file_fluid_properties = fluid_properties_data_check(path=FLUID_PROPERTIES_FILE_PATH)
+file_operative_state = operative_state_data_check(path=OPERATIVE_STATE_FILE_PATH)
--- utils/preprocessing.py
+++ utils/preprocessing.py
@@ -6,15 +6,16 @@ def hydrofoils_data_check(path: str):
     """
     Function for checking the availability of the hydrofoil data files.
     Function reports if the folder exists or not, and the number of files found.
-    In addition, reports if the files have the correct extension and data structure.+
+    In addition, reports if the files have the correct extension and data structure.
+
     :param path: str, path to the folder containing the hydrofoil data files.
-    :return: None
+    :return: files: list, list of the files found in the folder.
     """
     if os.path.exists(path):
         # Check  if the path exists and print the number of files found.
         print(f"Folder {path} exists. The path is correct. (\u2713)")
         files = os.listdir(path)
-        print(f"Number of files found: {len(files)}")
+        print(f"Number of files found: {len(files)} (i.e. design points).")
         # Check if the files have the correct extension.
         for file in files:
             if file.endswith(".yml"):
@@ -35,3 +36,60 @@ def hydrofoils_data_check(path: str):
         print(f"Folder {path} does not exist. Please check the path. (x)")
         return None
     return files
+
+
+def fluid_properties_data_check(path: str):
+    """
+    Function for checking the availability of the fluid properties data files.
+    Function reports if the folder exists or not, and if the data structure is correct.
+    :param path: str, path to the folder containing the fluid properties data files.
+    """
+    # Check if the path exists.
+    if os.path.exists(path):
+        print(f"File {path} exists. The path is correct. (\u2713)")
+        if path.endswith(".yml"):
+            print(f"File {path} has the correct extension. (\u2713)")
+            keys = ['density', 'kinematic_viscosity', 'dynamic_viscosity', 'salinity', 'temperature']
+            with open(path, 'r') as f:
+                data = yaml.safe_load(f)
+                if all(key in data.keys() for key in keys):
+                    print(f"File {path} has the correct data structure. (\u2713)")
+                else:
+                    print(f"File {path} does not have the correct data structure. Please check the file. (x)")
+                    return None
+        else:
+            print(f"File {path} does not have the correct extension. Please check the file. (x)")
+            return None
+    else:
+        print(f"File {path} does not exist. Please check the path. (x)")
+        return None
+    return path
+
+
+def operative_state_data_check(path: str):
+    """
+    Function for checking the availability of the operative state data files.
+    Function reports if the folder exists or not, and if the data structure is correct.
+    :param path: str, path to the folder containing the operative state data files.
+    """
+    # Check if the path exists.
+    if os.path.exists(path):
+        print(f"File {path} exists. The path is correct. (\u2713)")
+        if path.endswith(".yml"):
+            print(f"File {path} has the correct extension. (\u2713)")
+            keys = ['optimal_speed', 'tip_speed_ratio', 'angular_speed', 'rpm', 'blade_radius', 'no_blades',
+                    'radius_hub_pctg', 'initial_point_pctg', 'final_point_pctg', 'no_design_points', 'operative_reynolds']
+            with open(path, 'r') as f:
+                data = yaml.safe_load(f)
+                if all(key in data.keys() for key in keys):
+                    print(f"File {path} has the correct data structure. (\u2713)")
+                else:
+                    print(f"File {path} does not have the correct data structure. Please check the file. (x)")
+                    return None
+        else:
+            print(f"File {path} does not have the correct extension. Please check the file. (x)")
+            return None
+    else:
+        print(f"File {path} does not exist. Please check the path. (x)")
+        return None
+    return path
