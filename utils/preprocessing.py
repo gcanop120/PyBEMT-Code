b1195a1e68a24d8eb1fd73425b065bde326bdcd5
Gerardo Cano Perea <144932076+gcanop120@users.noreply.github.com>
2024-09-21 18:22:03 -0600

    Preprocessing file hydrofoil function verified

--- utils/preprocessing.py
+++ utils/preprocessing.py
@@ -1,4 +1,5 @@
 import os
+import yaml
 
 
 def hydrofoils_data_check(path: str):
@@ -10,12 +11,27 @@ def hydrofoils_data_check(path: str):
     :return: None
     """
     if os.path.exists(path):
-        print(f"Folder {path} exists. The path is correct.")
+        # Check  if the path exists and print the number of files found.
+        print(f"Folder {path} exists. The path is correct. (\u2713)")
         files = os.listdir(path)
         print(f"Number of files found: {len(files)}")
-        return None
+        # Check if the files have the correct extension.
+        for file in files:
+            if file.endswith(".yml"):
+                print(f"File {file} has the correct extension. (\u2713)")
+                # Check if the files have the correct data structure.
+                keys = ['alpha', 'cl', 'cd', 'cm', 'efficiency', 'hydrofoil', 'reynolds', 'source']
+                with open(f"{path}/{file}", 'r') as f:
+                    data = yaml.safe_load(f)
+                    if all(key in data.keys() for key in keys):
+                        print(f"File {file} has the correct data structure. (\u2713)")
+                    else:
+                        print(f"File {file} does not have the correct data structure. Please check the file. (x)")
+                        return None
+            else:
+                print(f"File {file} does not have the correct extension. Please check the file. (x)")
+                return None
     else:
-        print(f"Folder {path} does not exist. Please check the path.")
+        print(f"Folder {path} does not exist. Please check the path. (x)")
         return None
-
-
+    return files
